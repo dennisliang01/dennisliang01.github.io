f9e99f424e8ded65419c232eca0a94587b3ac491
Dennis Liang <dennisliang01@gmail.com>
2021-01-09 11:30:35 -0800

    finalizing the admin screen

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const output = document.querySelector("#output");
 const cutDeviceBtn = document.querySelector("#cut_device");
 const output2 = document.querySelector("#output2");
 let gdxDevice;
+var firebaseConfig = {
+  apiKey: "AIzaSyDREc2lp_WyDu7ntQ6M3UnxUzf_27Hf0bI",
+  authDomain: "weatherdashboard-3bdc4.firebaseapp.com",
+  databaseURL: "https://weatherdashboard-3bdc4-default-rtdb.firebaseio.com",
+  projectId: "weatherdashboard-3bdc4",
+  storageBucket: "weatherdashboard-3bdc4.appspot.com",
+  messagingSenderId: "80992445262",
+  appId: "1:80992445262:web:42d193e45d4d8919e6741f",
+  measurementId: "G-F9QVEGVE9Z"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var sendData = null; 
 
 
 const selectDevice = async () => {
@@ -14,7 +27,22 @@ const selectDevice = async () => {
   try {
     gdxDevice = await godirect.selectDevice();
     // print name and serial number
-    output.textContent = `\n Connected to ` + gdxDevice.name;
+    var stat = `\n Connected to ` + gdxDevice.name;
+    output.textContent = stat;
+
+    sendData = setInterval(async function(){
+  
+      var temp = parseFloat(document.getElementById("data").innerHTML);
+      if (isNaN(temp)){
+        return; 
+      }
+      console.log("Data was sent");
+      var dbRef =  firebase.database().ref();
+      const weatherRef = dbRef.child("table"); 
+      weatherRef.remove();
+      weatherRef.push({"temp":temp}); 
+  }, 500);
+  
 
     //turns on the Default sensor
     gdxDevice.enableDefaultSensors();
@@ -34,6 +62,7 @@ const selectDevice = async () => {
         });
       });
     });
+  
   } catch (err) {
     console.error(err);
   }
@@ -44,10 +73,12 @@ const cutDevice = async () => {
   try {
     gdxDevice.close();
     document.getElementById("data").innerHTML = "No Data";
+    clearInterval(sendData); 
     output.textContent = `\n Disconnected from ` + gdxDevice.name;
   } catch (err) {
     console.error(err);
   }
+  
 };
 
 selectDeviceBtn.addEventListener("click", selectDevice); //opens selection window displaying available Go Direct sensors
