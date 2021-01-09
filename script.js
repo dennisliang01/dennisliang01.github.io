b6d7f8321aa4257cb3a6aa3377a5cac3d1320353
Dennis Liang <dennisliang01@gmail.com>
2021-01-09 13:54:09 -0800

    format editing

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Dennis Liang
-// Last edited 12/1/20  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Last edited 1/9/21
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
   apiKey: "AIzaSyDREc2lp_WyDu7ntQ6M3UnxUzf_27Hf0bI",
   authDomain: "weatherdashboard-3bdc4.firebaseapp.com",
@@ -10,22 +12,16 @@ var firebaseConfig = {
   appId: "1:80992445262:web:42d193e45d4d8919e6741f",
   measurementId: "G-F9QVEGVE9Z"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
 
-
 setInterval(function(){
-
   getWeatherData();
-
-
 }, 5000);
 
-
-
-function getWeatherData(){
-  
+function getWeatherData(){  
   var dbRef =  firebase.database().ref();
   const weatherRef = dbRef.child("table"); 
   let dataLast = " temperature is loading..."; 
@@ -38,12 +34,9 @@ function getWeatherData(){
      console.log(dataLast); 
     });
   });
+
   document.querySelector("#output").innerHTML = dataLast;
   document.querySelector("#time").innerHTML = "This is the tempature at :" + new Date();
-
 }
 
 getWeatherData();
-
-
-
